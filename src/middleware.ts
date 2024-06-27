5b94d4e2f4c2bc5940f15d7b52954be93c3b2c99
Kunal Tajne <kunal.tajne96@gmail.com>
2024-06-27 17:43:11 -0400

    Bug fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,19 +7,19 @@ export const config = {
 };
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request });
-  const url = new URL(request.url);
-  const pathname = url.pathname;
+  // const token = await getToken({ req: request });
+  // const url = new URL(request.url);
+  // const pathname = url.pathname;
 
-  // If the user has a token and is trying to access the sign-in, sign-up, verify, or home page, redirect to the dashboard
-  if (token && (pathname.startsWith("/sign-in") || pathname.startsWith("/sign-up") || pathname.startsWith("/verify") || pathname === "/")) {
-    return NextResponse.redirect(new URL("/dashboard", url.origin));
-  }
+  // // If the user has a token and is trying to access the sign-in, sign-up, verify, or home page, redirect to the dashboard
+  // if (token && (pathname.startsWith("/sign-in") || pathname.startsWith("/sign-up") || pathname.startsWith("/verify") || pathname === "/")) {
+  //   return NextResponse.redirect(new URL("/dashboard", url.origin));
+  // }
 
-  // If the user doesn't have a token and is trying to access the dashboard, redirect to the sign-in page new changes and fixes
-  if (!token && pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/sign-in", url.origin));
-  }
+  // // If the user doesn't have a token and is trying to access the dashboard, redirect to the sign-in page new changes and fixes
+  // if (!token && pathname.startsWith("/dashboard")) {
+  //   return NextResponse.redirect(new URL("/sign-in", url.origin));
+  // }
 
   return NextResponse.next();
 }
