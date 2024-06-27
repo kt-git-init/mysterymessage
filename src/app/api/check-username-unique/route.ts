f7cf3903be9a081ecfae6b015692171553781830
Kunal Tajne <kunal.tajne96@gmail.com>
2024-06-27 17:15:49 -0400

    Suggested Messages Fixes

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -1,7 +1,8 @@
-import dbConnect from '@/lib/dbConnect';
-import UserModel from '@/model/User';
-import { z } from 'zod';
-import { usernameValidation } from '@/schemas/signUpSchema';
+"use client";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { z } from "zod";
+import { usernameValidation } from "@/schemas/signUpSchema";
 
 const UsernameQuerySchema = z.object({
   username: usernameValidation,
@@ -13,7 +14,7 @@ export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
     const queryParams = {
-      username: searchParams.get('username'),
+      username: searchParams.get("username"),
     };
 
     const result = UsernameQuerySchema.safeParse(queryParams);
@@ -23,10 +24,7 @@ export async function GET(request: Request) {
       return Response.json(
         {
           success: false,
-          message:
-            usernameErrors?.length > 0
-              ? usernameErrors.join(', ')
-              : 'Invalid query parameters',
+          message: usernameErrors?.length > 0 ? usernameErrors.join(", ") : "Invalid query parameters",
         },
         { status: 400 }
       );
@@ -43,7 +41,7 @@ export async function GET(request: Request) {
       return Response.json(
         {
           success: false,
-          message: 'Username is already taken',
+          message: "Username is already taken",
         },
         { status: 200 }
       );
@@ -52,16 +50,16 @@ export async function GET(request: Request) {
     return Response.json(
       {
         success: true,
-        message: 'Username is unique',
+        message: "Username is unique",
       },
       { status: 200 }
     );
   } catch (error) {
-    console.error('Error checking username:', error);
+    console.error("Error checking username:", error);
     return Response.json(
       {
         success: false,
-        message: 'Error checking username',
+        message: "Error checking username",
       },
       { status: 500 }
     );
